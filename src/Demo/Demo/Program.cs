1d7ff3e88de61a460c023fbe074f3658027e3706
Benmoussa Younes <bybenmoussa@gmail.com>
2023-12-16 14:22:44 +0100

    added jetbrains config files

---
+++ src/Demo/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Demo;
+
+var island = new string[5, 5]
+{
+    {"X", "X", "O", "O", "O"},
+    {"X", "x", "O", "O", "O"},
+    {"O", "O", "0", "X", "X"},
+    {"X", "X", "O", "O", "X"},
+    {"X", "X", "O", "O", "O"}
+};
+//pattern visitor
+var map = new Map();
+map.PopulateMap(island,5,5);
+var islands = map.CalculateIslands();
+Console.WriteLine($"Number of islands in the maps are : {islands}");
+ 
